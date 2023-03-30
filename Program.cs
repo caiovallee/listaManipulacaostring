08920e0ca57e7421d7d624e8ba2b3a9f99c59e9b
owent <owent@DESKTOP-8NO99AE>
2023-03-30 13:54:20 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaManipulaçãostring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva uma frase");
+            string fraseUsuario = Console.ReadLine();
+
+            string[] palavras = fraseUsuario.Split(' ');
+
+            string TitleCase = " ";
+
+            foreach (string letra in palavras)
+            {
+               TitleCase += char.ToUpper(letra[0]) + letra.Substring(1).ToLower() + " ";
+            }
+
+
+            Console.WriteLine(TitleCase);
+
+            Console.ReadLine();
+
+        }
+    }
+}
